b1d02a8ce39a8ed8a3240d216fead7a3cd4f466d
puppy1012 <puppy1012@naver.com>
2025-03-06 22:57:48 +0900

    테스트코드 작성

--- src/test/kotlin/com/chomyom/portfolio/domain/repository/ExperienceRepositoryTest.kt
+++ src/test/kotlin/com/chomyom/portfolio/domain/repository/ExperienceRepositoryTest.kt
@@ -1,7 +1,19 @@
 package com.chomyom.portfolio.domain.repository
 
+import com.chomyom.portfolio.domain.entity.Experience
+import org.junit.jupiter.api.TestInstance
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest
 
 @DataJpaTest
-class ExperienceRepositoryTest {
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+class ExperienceRepositoryTest(
+    @Autowired val experienceRepository:ExperienceRepository
+) {
+    val DATA_SIZE=10
+
+    private fun createExperience(n:Int):Experience{
+
+    }
+
 }
